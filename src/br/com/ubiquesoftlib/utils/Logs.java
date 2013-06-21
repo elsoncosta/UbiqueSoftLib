385aa77f021e1eed0f1596fdb8c853d25081efae
Elson Costa <=>
2013-06-21 17:50:15 -0300

    atualizações

--- src/br/com/ubiquesoftlib/utils/Logs.java
+++ src/br/com/ubiquesoftlib/utils/Logs.java
@@ -8,11 +8,11 @@ public class Logs
 {
 	public static <T> void LogError(Class<T> type, String e)
 	{
-		Log.i(type.getName(), new StringBuilder().append(R.string.error).append(e).toString());
+		Log.e(type.getName(), new StringBuilder().append(R.string.error).append(e).toString());
 	}
 	
 	public static void LogError(String tag, String e)
 	{
-		Log.i(tag, new StringBuilder().append(R.string.error).append(e).toString());
+		Log.e(tag, new StringBuilder().append(R.string.error).append(e).toString());
 	}
 }
